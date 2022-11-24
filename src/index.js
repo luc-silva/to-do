5da029173ce17609c4d14b5f467d8fcc05ba5885
Lucas Santos <lucas.silva1203@outlook.com>
2022-11-24 14:14:26 -0300

    Modify initializeTasktab function and fix listener
    initializeTasktab now also render buttons and a div to contain todo
    cards. Fixed addEventListeners that didn't trigger the tasktab function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import initializeTasksTab from "./modules/tasktab.js"
+import initializeProjectTab from "./modules/project.js"
+import initializeTaskTab from "./modules/tasktab.js"
 
-let todoPanel = function(){
-    let dashboardBtn = document.querySelector("dashboard-btn")
-    let projectsBtn = document.querySelector("projects-btn")
+let todoPanel = (function(){
+    let dashboardBtn = document.querySelector("#dashboard-btn")
+    let projectsBtn = document.querySelector("#projects-btn")
+    projectsBtn.addEventListener("click", initializeProjectTab)
 
 
-    let tasksBtn = document.querySelector("tasks-btn")
-    tasksBtn.addEventListener("click", initializeTasksTab)
-}
+    let tasksBtn = document.querySelector("#tasks-btn")
+    tasksBtn.addEventListener("click", initializeTaskTab)
+
+})()
--- src/modules/tasktab.js
+++ src/modules/tasktab.js
@@ -1,61 +1,95 @@
 let tasktabDomManipulator = (function(){
+    let taskCreatorScreen = document.querySelector("#task-creator-screen")
+
     let createDiv = function(){
         return document.createElement("div")
+    }
+    let createBtn = function(){
+        return document.createElement("button")
+    }
+
+    //Add task popups
+    let backgroundPopup = document.querySelector("#popup-background")
+    let addTaskBtn = document.querySelector("#add-task-btn")
+
+    backgroundPopup.addEventListener("click", () => {
+        removeTaskCreatorPopup()
+    })
+    addTaskBtn.addEventListener("click", () => {
+        showTaskCreatorPopup()
+    })
+    
+    let showTaskCreatorPopup = function(){
+        backgroundPopup.style.display = "block"
+        taskCreatorScreen.style.display = "flex"
+    }
+    
+    let removeTaskCreatorPopup = function(){
+        backgroundPopup.style.display = "none"
+        taskCreatorScreen.style.display = "none"
+    }       
+
+    //structure functions
+    let createTaskAddBtn = function(){
+        let div = createDiv()
+        div.id = "tasks-button-panel"
+
+        let button = createBtn()
+        button.id = "add-task-btn"
+        button.textContent = "Add task"
+
+        button.addEventListener("click", () => {
+            showTaskCreatorPopup()
+        })
+        
+        div.append(button)
+        return div
+    }
+    let createTaskContainer = function(){
+        let tasksContainer = document.createElement("div")
+        tasksContainer.id = "tasks-container"
+
+        return tasksContainer
     }
     
     let createTodoElement = function(){
         let todoCard = document.createElement("span")
         todoCard.classList.add("todo-card")
-
+        
         let cardTask = createDiv()
         cardTask.classList.add("card-task")
         cardTask.innerHTML = 'Task:<h3>Brush my Tooth</h3>'
-
+        
         let todoCardDetails = createDiv()
         todoCardDetails.classList.add("todo-card-details")
         todoCardDetails.innerHTML = `
         <div class="card-dealine"><strong>Deadline</strong>: 21/32/3213</div>
         <div class="card-details-btn">Details</div>`
-
-        todoCard.append(cardTask, todoCardDetails)
-
-        return todoCard
-
-    }
-    showTaskCreatorScreen = function(){
         
+        todoCard.append(cardTask, todoCardDetails)
+        
+        return todoCard
     }
-
-    return {createTodoElement}
+    
+    return {createTodoElement, createTaskContainer, createTaskAddBtn}
 })()
 
-let createTab = (function (){
-    let background = document.querySelector("#popup-background")
-    background.addEventListener("click", () => {
-        background.style.display = "none"
-    })
+function initializeTaskTab(){
+    let tasktab = document.querySelector("#todo-display")
+    tasktab.textContent = ""
+    //func para criar botoes
 
-    let addTaskBtn = document.querySelector("#add-task-btn")
-    addTaskBtn.addEventListener("click",() => {
-        background.style.display = "block"
-        showTaskCreatorScreen()
-    })
+    tasktab.append(tasktabDomManipulator.createTaskAddBtn())
 
-    //jogar no dom mani
-    let todoDisplay = document.querySelector("#todo-display")
+    let tasksContainer = tasktabDomManipulator.createTaskContainer()
+    tasktab.append(tasksContainer)
+
     let todosArray = [1, 2, 3, 4, 5, 6]
     todosArray.forEach(todo => {
         console.log(todo)
-        todoDisplay.appendChild(tasktabDomManipulator.createTodoElement())
+        tasksContainer.append(tasktabDomManipulator.createTodoElement())
     })
 
-})()
-
-
-
-function initializeTaskTab(){
-    let mainScreen = document.querySelector("todo-display")
-    mainScreen.appendChild()
 }
 
 export default initializeTaskTab;
