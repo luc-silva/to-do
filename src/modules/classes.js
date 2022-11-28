e458a8a3085ae75c611a753c1d57ed4d33f63d57
Lucas Santos <lucas.silva1203@outlook.com>
2022-11-28 18:19:57 -0300

    Add classes

---
+++ src/modules/classes.js
@@ -0,0 +1,66 @@
+let user = (function () {
+	let todoArray = [];
+	let projectArray = [];
+
+	return { todoArray };
+})();
+
+class Project {
+	constructor(title, description, dateCreated) {
+		this.title = title;
+		this.description = description;
+		this.dateCreated = new Date();
+	}
+}
+
+class ProjectTask {
+	constructor(title, priority) {
+		this.title = title;
+		this.priority = priority;
+	}
+
+	get title() {
+		return this._title;
+	}
+	get priority() {
+		return this._priority;
+	}
+}
+
+class Todo {
+	constructor(task, description, deadline, priority, check) {
+		this.task = task;
+		this.description = description;
+		this.deadline = deadline;
+		this.priority = priority;
+		this.check = check;
+	}
+	dateCreated = new Date();
+
+	get dateCreated() {
+		return this._dateCreated;
+	}
+
+	get deadline() {
+		return this._deadline;
+	}
+	set deadline(newDate) {
+		this._deadline = newDate;
+	}
+
+	get priority() {
+		return this._priority;
+	}
+	set priority(newPriority) {
+		this._priority = newPriority;
+	}
+
+	get check() {
+		return this._check;
+	}
+	set check(value) {
+		this._check = value;
+	}
+}
+
+export default { user, Project, ProjectTask, Todo };
