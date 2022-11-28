2ee1d09eeba414c57c6fc0f49e9a2fb645d3c2b3
Lucas Santos <lucas.silva1203@outlook.com>
2022-11-28 17:38:45 -0300

    Move object user from tasktab to index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import initializeProjectTab from "./modules/project.js";
 import initializeTaskTab from "./modules/tasktab.js";
 
+let user = (function () {
+	let todoArray = [];
+
+	return { todoArray };
+})();
+
 let todoPanel = (function () {
 	let dashboardBtn = document.querySelector("#dashboard-btn");
 
@@ -10,3 +16,5 @@ let todoPanel = (function () {
 	let tasksBtn = document.querySelector("#tasks-btn");
 	tasksBtn.addEventListener("click", initializeTaskTab);
 })();
+
+export default user;
