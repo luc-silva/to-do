46b7958c42bced6c9c7a64188440a42ac357a736
Lucas Santos <lucas.silva1203@outlook.com>
2022-12-04 19:35:55 -0300

    Add dashboard tab listener

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import initializeDashboard from "./modules/dashboard.js";
 import initializeProjectTab from "./modules/project.js";
 import initializeTaskTab from "./modules/tasktab.js";
 
 let todoPanel = (function () {
 	let dashboardBtn = document.querySelector("#dashboard-btn");
+	dashboardBtn.addEventListener("click", initializeDashboard)
 
 	let projectsBtn = document.querySelector("#projects-btn");
 	projectsBtn.addEventListener("click", initializeProjectTab);
